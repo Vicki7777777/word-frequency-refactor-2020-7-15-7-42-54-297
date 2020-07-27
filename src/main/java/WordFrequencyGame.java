a8667458a81c15b1e14a00aea9000a2ebb13ddf6
Vicki <458044962@qq.com>
2020-07-27 20:36:49 +0800

    [WordFrequencyGame][Vicki]Refactor： Refactor the method of generateWordFrequencySentence

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -6,37 +6,21 @@ public class WordFrequencyGame {
     private static final String DELIMITER = "\n";
     private static final String BLANK_SPACE = " ";
     private String SEPARATOR = "\\s+";
+
     public String getResult(String sentence) {
         if (sentence.split(SEPARATOR).length == 1) {
             return sentence + WORD_COUNT;
         } else {
             try {
                 List<WordInfo> wordInfoList = generateWordInfos(sentence);
-
-                Map<String, List<WordInfo>> map = getListMap(wordInfoList);
-
-                List<WordInfo> list = new ArrayList<>();
-                for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()) {
-                    WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
-                    list.add(wordInfo);
-                }
-                wordInfoList = list;
-
-                wordInfoList.sort((firstWordInfo, secondeWordInfo) -> secondeWordInfo.getWordCount() - firstWordInfo.getWordCount());
-
-                StringJoiner joiner = new StringJoiner(DELIMITER);
-                for (WordInfo w : wordInfoList) {
-                    String s = w.getValue() + BLANK_SPACE + w.getWordCount();
-                    joiner.add(s);
-                }
-                return joiner.toString();
+                return generateWordFrequencySentence(wordInfoList);
             } catch (Exception e) {
                 return CALCULATE_ERROR;
             }
         }
     }
 
-    public  List<WordInfo> generateWordInfos(String sentence){
+    private List<WordInfo> generateWordInfos(String sentence) {
         List<WordInfo> wordInfos = new ArrayList<>();
         String[] wordArray = sentence.split(SEPARATOR);
         for (String words : wordArray) {
@@ -44,6 +28,23 @@ public class WordFrequencyGame {
             wordInfos.add(wordInfo);
         }
         return wordInfos;
+    }
+
+    private String generateWordFrequencySentence(List<WordInfo> wordInfos) {
+        Map<String, List<WordInfo>> map = getListMap(wordInfos);
+
+        List<WordInfo> list = new ArrayList<>();
+        for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()) {
+            WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
+            list.add(wordInfo);
+        }
+        wordInfos = list;
+
+        wordInfos.sort((firstWordInfo, secondeWordInfo) -> secondeWordInfo.getWordCount() - firstWordInfo.getWordCount());
+
+        StringJoiner joiner = new StringJoiner(DELIMITER);
+        return  joiner.toString();
+
     }
 
     private Map<String, List<WordInfo>> getListMap(List<WordInfo> wordInfoList) {
